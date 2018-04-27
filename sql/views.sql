27ca88dd97151f5edbf46b58479d56d2e787c687
John Kusner <jkusner321@gmail.com>
2018-04-26 20:53:25 -0400

    New warehouse_stock view
    merges all warehouse stock information into an average, qty
    in the (near!) future should enforce pricing to be consistent accross
    warehouses

--- sql/views.sql
+++ sql/views.sql
@@ -4,3 +4,14 @@ select product_id, sum(qty * unit_price) as total_sales, sum(qty) as amount_sold
 from purchased natural join product
 group by product_id, product_name
 order by amount_sold desc
+
+create or replace view warehouse_stock
+as
+select product_id, product_name, qty, unit_price
+from (
+    select product_id, sum(qty) as qty, avg(unit_price) as unit_price
+    from stock natural join location
+    where location_id in (select location_id from warehouse)
+    group by product_id
+)
+natural join product
